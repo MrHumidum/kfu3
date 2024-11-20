1fc6783cc10523acdf0c4a4e5635f6a979196339
MrHumidum <salavatcool5@gmail.com>
2024-11-20 14:17:26 +0300

    done

---
+++ Homework3/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab_2
+{
+    class Program
+    {
+        enum DayOfTheWeek
+        {
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7
+        }
+        static void Ex1()
+        {
+            int temp = 0;
+            bool flag = true;
+            int[] numbers = new int[10];
+            Console.WriteLine("Введите 10 цифр через enter(каждое новое число на новой строке)");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (int.TryParse(Console.ReadLine(), out int num))
+                {
+                    numbers[i] = num;
+                }
+                else
+                {
+                    Console.WriteLine("Неправильный ввод цифры");
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (numbers[i] > numbers[i + 1])
+                {
+                    Console.WriteLine($"Число под номером {i + 2} нарушает последовательность");
+                    break;
+                }
+            }
+        }
+
+        static void Ex2()
+        {
+            Console.Write("Введите порядковый номер карты (от 6 до 14): ");
+            string cardValue = string.Empty;
+            try
+            {
+                if (int.TryParse(Console.ReadLine(), out int k) && (k >= 6 && k <= 14))
+                {
+                    switch (k)
+                    {
+                        case 6:
+                            cardValue = "Шестёрка";
+                            break;
+                        case 7:
+                            cardValue = "Семёрка";
+                            break;
+                        case 8:
+                            cardValue = "Восьмёрка";
+                            break;
+                        case 9:
+                            cardValue = "Девятка";
+                            break;
+                        case 10:
+                            cardValue = "Десятка";
+                            break;
+                        case 11:
+                            cardValue = "Валет";
+                            break;
+                        case 12:
+                            cardValue = "Дама";
+                            break;
+                        case 13:
+                            cardValue = "Король";
+                            break;
+                        case 14:
+                            cardValue = "Туз";
+                            break;
+                        default:
+                            cardValue = k.ToString();
+                            break;
+                    };
+                    Console.WriteLine($"Достоинство карты: {cardValue}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неправильный ввод");
+            }
+            finally
+            {
+                Console.WriteLine("Программа завершена");
+            }
+        }
+        static void Ex3()
+        {
+            Console.Write("Введите входные данные: ");
+            string input = Console.ReadLine();
+            input = string.Join(" ", input.ToLower().Split(" ").Select(s => char.ToUpperInvariant(s[0]) + s.Substring(1)));
+            Console.WriteLine(input);
+            switch (input)
+            {
+                case "Jabroni":
+                    Console.WriteLine("Patron Tequilla");
+                    break;
+                case "School Counselor":
+                    Console.WriteLine("Patron Tequilla");
+                    break;
+                case "Programmer":
+                    Console.WriteLine("Hipsters Craft Beer");
+                    break;
+                case "Bike Gang Member":
+                    Console.WriteLine("Moonshine");
+                    break;
+                case "Politician":
+                    Console.WriteLine("Your tax dollars");
+                    break;
+                case "Rapper":
+                    Console.WriteLine("Cristal");
+                    break;
+                default:
+                    Console.WriteLine("Beer");
+                    break;
+
+            }
+
+        }
+
+        static void Ex4()
+        {
+            Console.Write("Введите порядковый номер дня недели: ");
+            if (int.TryParse(Console.ReadLine(), out int num) && (num > 0 && num < 8))
+            {
+                Console.WriteLine((DayOfTheWeek)num);
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ввод порядкового номера дня недели");
+            }
+        }
+
+        static void Ex5()
+        {
+            string[] toys = { "Teddy Bear", "Hello Kitty", "Toy Car", "Barbie doll", "LEGO Set", "Hello Kitty", "Hello Kitty", "Hello Kitty", "Hello Kitty" };
+
+            int dollCount = 0;
+            string bag = "";
+
+            foreach (string toy in toys)
+            {
+                if (toy == "Hello Kitty" || toy == "Barbie doll")
+                {
+                    bag += toy + ", ";
+                    dollCount++;
+                }
+            }
+            if (bag.Length > 0)
+            {
+                bag = bag.Substring(0, bag.Length - 2);
+            }
+            Console.WriteLine($"В сумке {dollCount} кукол: {bag}");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("№1");
+            /*Дана последовательность из 10 чисел. Определить, является ли эта последовательность
+упорядоченной по возрастанию. В случае отрицательного ответа определить
+порядковый номер первого числа, которое нарушает данную последовательность.
+Использовать break.*/
+            // Ex1();
+
+
+            Console.WriteLine("№2");
+            /*Игральным картам условно присвоены следующие порядковые номера в зависимости от
+их достоинства: «валету» — 11, «даме» — 12, «королю» — 13, «тузу» — 14.
+Порядковые номера остальных карт соответствуют их названиям («шестерка»,
+«девятка» и т. п.). По заданному номеру карты k (6 <=k <= 14) определить достоинство
+соответствующей карты. Использовать try-catch-finally.*/
+            // Ex2();
+
+            Console.WriteLine("№3");
+            /*Напишите программу, которая принимает на вход е строку и производит выходные
+данные в соответствии со следующей таблицей:
+Примечание. По умолчанию используется “все остальное”: если входные данные
+функции не соответствуют ни одному из значений в таблице, то возвращаемое значение
+должно быть «Beer».
+Убедитесь, что вы охватили случаи, когда некоторые слова не отображаются с
+правильным написанием заглавных букв. Например, ввод «pOLitiCIaN» должен
+по-прежнему возвращать «Your tax dollars».*/
+            // Ex3();
+
+            Console.WriteLine("№4");
+            /*Составить программу, которая в зависимости от порядкового номера дня недели (1, 2,
+...,7) выводит на экран его название (понедельник, вторник, ..., воскресенье).
+Использовать enum.*/
+            Ex4();
+
+            Console.WriteLine("№5");
+            /*Создать массив строк. При помощи foreach обойти весь массив. При встрече элемента
+"Hello Kitty" или "Barbie doll" необходимо положить их в “сумку”, т.е. прибавить к
+результату. Вывести на экран сколько кукол в “сумке”.*/
+            Ex5();
+
+
+        }
+    }
+
+
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+
+        static void Ex1()
+        {
+            Console.Write("Введите номер дня в году: ");
+            if (double.TryParse(Console.ReadLine(), out double numOfDay))
+            {
+                DateTime date = new DateTime(2015, 1, 1);
+                date = date.AddDays(numOfDay - 1);
+                Console.WriteLine($"{date:M}");
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ввод номерa дня");
+            }
+        }
+
+        static void Ex2()
+        {
+            Console.Write("Введите номер дня в году: ");
+            if (double.TryParse(Console.ReadLine(), out double numOfDay) && (numOfDay > 0 && numOfDay < 366))
+            {
+                DateTime date = new DateTime(2015, 1, 1);
+                date = date.AddDays(numOfDay - 1);
+                Console.WriteLine($"{date:M}");
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ввод номерa дня");
+            }
+
+        }
+
+        static void Ex3()
+        {
+            Console.Write("Введите год: ");
+            if (int.TryParse(Console.ReadLine(), out int year))
+            {
+                Console.Write("Введите номер дня в году: ");
+                if (double.TryParse(Console.ReadLine(), out double numOfDay) && (numOfDay > 0 && numOfDay < 366))
+                {
+                    DateTime date = new DateTime(year, 1, 1);
+                    date = date.AddDays(numOfDay - 1);
+                    Console.WriteLine(DateTime.IsLeapYear(year) ? $"Високосный год, {date:M}" : $"Не високосный год, {date:M}");
+                }
+                else
+                {
+                    Console.WriteLine("Неправильный ввод номерa дня");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ввод номерa дня");
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 4.1");
+            /*Написать программу, которая читает с экрана число от 1 до 365 (номер дня
+    в году), переводит этот число в месяц и день месяца. Например, число 40 соответствует 9
+    февраля (високосный год не учитывать).*/
+            Ex1();
+
+            Console.WriteLine("Упражнение 4.2");
+            /*Добавить к задаче из предыдущего упражнения проверку числа введенного
+    пользователем. Если число меньше 1 или больше 365, программа должна вырабатывать
+    исключение, и выдавать на экран сообщение.*/
+            Ex2();
+
+            Console.WriteLine("Домашнее задание 4.1");
+            /*Изменить программу из упражнений 4.1 и 4.2 так, чтобы она
+    учитывала год (високосный или нет). Год вводится с экрана. (Год високосный, если он
+    делится на четыре без остатка, но если он делится на 100 без остатка, это не високосный
+    год. Однако, если он делится без остатка на 400, это високосный год.)*/
+            Ex3();
+        }
+    }
+
+
+}
